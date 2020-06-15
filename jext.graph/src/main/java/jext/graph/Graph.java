ca4ed813171df5b31eecef2619e9f632549c1491
Corrado Mio <corrado.mio@unimi.it>
2020-06-15 17:21:44 +0400

    Update implementation

--- jext.graph/src/main/java/jext/graph/Graph.java
+++ jext.graph/src/main/java/jext/graph/Graph.java
@@ -1,4 +1,9 @@
 package jext.graph;
 
-public interface Graph {
+public interface Graph<V, E> {
+
+    int addVertex(V vertex);
+    E newEdge(V source, V target);
+    E newEdge(int source, int target);
+    int addEdge(E edge);
 }
