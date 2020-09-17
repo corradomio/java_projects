262456863e959ea3dd5821faca59b3efd0d8cb2f
Corrado Mio <corrado.mio@unimi.it>
2020-09-17 16:06:38 +0400

    Updated implementation

--- check_java/src/main/java/org/hls/check/Main.java
+++ check_java/src/main/java/org/hls/check/Main.java
@@ -1,9 +1,18 @@
 package org.hls.check;
 
-import java.lang.ref.WeakReference;
+import jext.util.JarUtils;
+
+import java.io.File;
+
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+
+        System.out.println(JarUtils.containsClass(
+            new File("D:\\Starspace\\Users\\Corrado Mio\\.spl\\.extlib\\androidx\\appcompat\\appcompat\\1.0.0\\appcompat-1.0.0.aar"),
+            "androidx.appcompat.app.ActionBar"
+        ));
+
 
     }
 }
--- jext.commons/src/main/java/jext/util/JarUtils.java
+++ jext.commons/src/main/java/jext/util/JarUtils.java
@@ -1,26 +1,236 @@
 package jext.util;
 
+import jext.logging.Logger;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 
 public class JarUtils {
+
+    // ----------------------------------------------------------------------
+    // Archive analyzers
+    // ----------------------------------------------------------------------
+
+    private interface FileAnalyzer {
+        boolean exists();
+        boolean containsClass(String className);
+        Set<String> listClassNames();
+    }
+
+    private static abstract class BaseAnalyzer implements FileAnalyzer {
+        protected File libFile;
+
+        protected BaseAnalyzer(File libFile) {
+            this.libFile = libFile;
+        }
+
+        @Override
+        public boolean exists() {
+            return libFile.exists();
+        }
+
+        @Override
+        public boolean containsClass(String className) {
+            String entryName = asEntryName(className);
+
+            // check for a top class
+            try (JarFile jarFile = new JarFile(libFile)) {
+                ZipEntry entry = jarFile.getEntry(entryName);
+                if (entry != null)
+                    return true;
+            }
+            catch (IOException e) {}
+
+            // check for an inner class (containing '$')
+            try (JarFile jarFile = new JarFile(libFile)) {
+                ZipEntry entry;
+                Enumeration<JarEntry> e = jarFile.entries();
+                while (e.hasMoreElements()) {
+                    entry = e.nextElement();
+                    if (entry != null && !entry.isDirectory() && entry.getName().endsWith(DOT_CLASS)) {
+                        String thisName = entry.getName().replace('$', '/');
+                        if (thisName.equals(entryName))
+                            return true;
+                    }
+                }
+            }
+            catch (IOException e) {}
+
+            // not found
+            return false;
+        }
+
+        @Override
+        public Set<String> listClassNames() {
+            return listFileNames(libFile, DOT_CLASS)
+                .stream()
+                .map(this::asClassName)
+                .collect(Collectors.toSet());
+        }
+
+        protected abstract String asEntryName(String className);
+
+        protected abstract String asClassName(String entryName);
+    }
+
+    private static class JarAnalyzer extends BaseAnalyzer {
+
+        protected JarAnalyzer(File file) {
+            super(file);
+        }
+
+        @Override
+        protected String asEntryName(String className) {
+            return className.replace('.', '/') + DOT_CLASS;
+        }
+
+        @Override
+        protected String asClassName(String entryName) {
+            // strip end ".class"
+            return entryName.substring(0, entryName.length() - DOT_CLASS.length())
+                .replace('/', '.')
+                .replace('$', '.');
+        }
+    }
+
+    private static class JModAnalyzer extends BaseAnalyzer {
+
+        protected JModAnalyzer(File file) {
+            super(file);
+        }
+
+        @Override
+        protected String asEntryName(String className) {
+            return CLASSES + className.replace('.', '/') + DOT_CLASS;
+        }
+
+        @Override
+        protected String asClassName(String entryName) {
+            // strip end ".class"
+            return entryName.substring(CLASSES.length(), entryName.length() - DOT_CLASS.length())
+                .replace('/', '.')
+                .replace('$', '.');
+        }
+    }
+
+    // ----------------------------------------------------------------------
+    // ".aar" archive
+    // ----------------------------------------------------------------------
+    // it is a "zip" file
+    // the classes are saved in "<zip>/classes.jar"
+
+    private static class AarAnalyzer implements FileAnalyzer {
+
+        private File libFile;
+
+        protected AarAnalyzer(File libFile) {
+            this.libFile = libFile;
+        }
+
+        @Override
+        public boolean exists() {
+            return libFile.exists();
+        }
+
+        @Override
+        public boolean containsClass(String className) {
+
+            try(ZipFile zfile = new ZipFile(libFile)) {
+                ZipEntry classes = zfile.getEntry("classes.jar");
+                try(JarInputStream is = new JarInputStream(zfile.getInputStream(classes))) {
+                    ZipEntry ze = is.getNextEntry();
+                    while (ze != null) {
+                        if (ze.getName().endsWith(DOT_CLASS)) {
+                            String thisName = asClassName(ze.getName());
+                            if (className.equals(thisName))
+                                return true;
+                        }
+                        ze = is.getNextEntry();
+                    }
+                }
+            }
+            catch(IOException e) {
+                return false;
+            }
+
+            return false;
+        }
+
+        private String asClassName(String entryName) {
+            // strip end ".class"
+            return entryName.substring(0, entryName.length() - DOT_CLASS.length())
+                .replace('/', '.')
+                .replace('$', '.');
+        }
+
+        @Override
+        public Set<String> listClassNames() {
+            Set<String> classNames = new HashSet<>();
+
+            try(ZipFile zfile = new ZipFile(libFile)) {
+                ZipEntry classes = zfile.getEntry("classes.jar");
+                try(JarInputStream is = new JarInputStream(zfile.getInputStream(classes))) {
+                    ZipEntry ze = is.getNextEntry();
+                    while (ze != null) {
+                        if (ze.getName().endsWith(DOT_CLASS)) {
+                            String thisName = asClassName(ze.getName());
+                            classNames.add(thisName);
+                        }
+                        ze = is.getNextEntry();
+                    }
+                }
+            }
+            catch(IOException e) {
+                return classNames;
+            }
+
+            return classNames;
+        }
+    }
+
+    // ----------------------------------------------------------------------
+
+    private static FileAnalyzer newAnalyzer(File libFile) {
+        String name = libFile.getName();
+        if (name.endsWith(DOT_AAR))
+            return new AarAnalyzer(libFile);
+        if (name.endsWith(DOT_JMOD))
+            return new JModAnalyzer(libFile);
+        else
+            return new JarAnalyzer(libFile);
+    }
+
 
     // ----------------------------------------------------------------------
     // Constants
     // ----------------------------------------------------------------------
 
-    public static final String DOT_JMOD = ".jmod";
-    public static final String DOT_CLASS = ".class";
-    public static final String DOT_JAVA = ".java";
+    private static Logger logger = Logger.getLogger(JarUtils.class);
+
+    // ----------------------------------------------------------------------
+    // Constants
+    // ----------------------------------------------------------------------
+
+    private static final String DOT_AAR = ".aar";
+    private static final String DOT_JAR = ".jar";
+    private static final String DOT_JMOD = ".jmod";
+    private static final String DOT_CLASS = ".class";
     private static final String CLASSES = "classes/";
 
     // ----------------------------------------------------------------------
@@ -28,34 +238,23 @@ public class JarUtils {
     // ----------------------------------------------------------------------
 
     public static boolean containsClass(File libFile, String className) {
-        if (!libFile.exists())
+
+        FileAnalyzer fa = newAnalyzer(libFile);
+
+        if (!fa.exists())
             return false;
-
-        String entryName = className.replace('.', '/') + ".class";
-        if (libFile.getName().endsWith(DOT_JMOD))
-            entryName = "classes/" + entryName;
-
-        try (JarFile jarFile = new JarFile(libFile)) {
-            ZipEntry entry = jarFile.getEntry(entryName);
-            return entry != null;
-        }
-        catch (IOException e) {}
-
-        return false;
+        else
+            return fa.containsClass(className);
     }
 
     public static Set<String> listClassNames(File libFile) {
-        if (!libFile.exists())
-            return Collections.emptySet();
 
-        boolean jmod = libFile.getName().endsWith(DOT_JMOD);
-        return listFileNames(libFile, DOT_CLASS)
-            .stream()
-            .map(path -> (jmod) ? path.substring(CLASSES.length()) : path)
-            .map(path -> path.substring(0, path.length()-DOT_CLASS.length())
-                .replace('/', '.')
-                .replace('$', '.'))
-            .collect(Collectors.toSet());
+        FileAnalyzer fa = newAnalyzer(libFile);
+
+        if (!fa.exists())
+            return Collections.emptySet();
+        else
+            return fa.listClassNames();
     }
 
     /**
@@ -65,11 +264,12 @@ public class JarUtils {
      * @param dotExt extension (with '.')
      * @return
      */
-    public static List<String> listFileNames(File libFile, String dotExt) {
+    private static List<String> listFileNames(File libFile, String dotExt) {
         if (!libFile.exists())
             return Collections.emptyList();
 
         List<String> names = new ArrayList<>();
+        logger.debugf("Analyzing %s", libFile.getAbsolutePath());
 
         try (JarFile jarFile = new JarFile(libFile)) {
             JarEntry entry;
@@ -88,88 +288,30 @@ public class JarUtils {
     }
 
     // ----------------------------------------------------------------------
-    // Count file extensions
+    // Extract jar from aar
     // ----------------------------------------------------------------------
 
-    // /**
-    //  * Count the file extensions present in the jar|zip file.
-    //  * If the compressed file contains
-    //  * - a lot of .class, it is a java library
-    //  * - a lot of .java, it is a compressed java source code
-    //  * - a lot of .html, it is a compressed javadoc
-    //  *
-    //  * @param jarFile
-    //  * @return
-    //  */
-    // public static Map<String,Integer> countFileExtensions(File jarFile) {
-    //     Map<String,Integer> count = new HashMap<>();
-    //
-    //     logger.infof("Analyzing %s", jarFile.getAbsolutePath());
-    //     try(ZipInputStream zip = new ZipInputStream(new FileInputStream(jarFile))) {
-    //         for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
-    //             logger.debugft("    analyzing %s", entry.getName());
-    //             if (!entry.isDirectory()) {
-    //                 String ext = extensionOf(entry.getName());
-    //                 int n = count.getOrDefault(ext, 0);
-    //                 count.put(ext, n+1);
-    //             }
-    //         }
-    //     }
-    //     catch (Exception e) {
-    //         logger.error("Unable to analyze " + jarFile, e);
-    //     }
-    //
-    //     return count;
-    // }
-    //
-    // private static String extensionOf(String name) {
-    //     int pos = name.lastIndexOf(".");
-    //     return pos != -1 ? name.substring(pos+1) : "";
-    // }
+    public static File extractJarFromAar(File aarArchive) {
+        File jarArchive = new File(aarArchive.getParentFile(), FileUtils.getNameWithoutExt(aarArchive) + ".jar");
+        if (jarArchive.exists() || !aarArchive.exists())
+            return jarArchive;
 
-    // ----------------------------------------------------------------------
-    // Open stream
-    // ----------------------------------------------------------------------
+        try(ZipFile zfile = new ZipFile(aarArchive)) {
+            ZipEntry classes = zfile.getEntry("classes.jar");
+            try(InputStream  is = zfile.getInputStream(classes);
+                OutputStream os = new FileOutputStream(jarArchive)) {
+                copy(is, os);
+            }
+        }
+        catch(IOException e) { }
 
-    // /**
-    //  * Return as InputStream the specified entry in the zip file
-    //  *
-    //  * @param jarFile compressed file
-    //  * @param entryName entry to select
-    //  * @return an InputStream
-    //  * @throws IOException
-    //  */
-    // public static InputStream openInputStream(File jarFile, String entryName) throws IOException {
-    //     ZipInputStream zip = new ZipInputStream(new FileInputStream(jarFile));
-    //     for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
-    //         if (entry.isDirectory())
-    //             continue;
-    //
-    //         if (entry.getName().equals(entryName))
-    //             return new InputStream() {
-    //                 @Override
-    //                 public int read() throws IOException {
-    //                     return zip.read();
-    //                 }
-    //
-    //                 @Override
-    //                 public int read(byte b[], int off, int len) throws IOException {
-    //                     return zip.read(b, off, len);
-    //                 }
-    //
-    //                 @Override
-    //                 public void close() {
-    //                     try {
-    //                         zip.close();
-    //                     }
-    //                     catch (IOException e) {
-    //                         logger.errorf("openInputStream(%s, %s): %s", jarFile, entryName, e);
-    //                     }
-    //                 }
-    //             };
-    //     }
-    //     zip.close();
-    //     throw new FileNotFoundException(String.format("%s!%s", jarFile, entryName));
-    // }
+        return jarArchive;
+    }
+
+    private static void copy(InputStream is, OutputStream os) throws IOException {
+        byte[] buffer = new byte[1024];
+        for (int n = is.read(buffer); n > 0; n = is.read(buffer))
+            os.write(buffer, 0, n);
+    }
 
 }
