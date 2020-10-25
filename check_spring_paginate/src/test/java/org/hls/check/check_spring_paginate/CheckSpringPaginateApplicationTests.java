0fc2a9de43db99c2c883ba4ed866937005b8455d
Corrado Mio <corrado.mio@gmail.com>
2020-10-25 15:56:52 +0400

    Updated implementation

---
+++ check_spring_paginate/src/test/java/org/hls/check/check_spring_paginate/CheckSpringPaginateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hls.check.check_spring_paginate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckSpringPaginateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
