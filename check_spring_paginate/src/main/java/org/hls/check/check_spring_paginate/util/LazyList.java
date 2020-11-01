8c8d7d38f1cb9a15117bb7e4207c9db882fa1af3
Corrado Mio <corrado.mio@gmail.com>
2020-11-01 08:01:52 +0400

    Updated implementation

--- check_spring_paginate/src/main/java/org/hls/check/check_spring_paginate/util/LazyList.java
+++ check_spring_paginate/src/main/java/org/hls/check/check_spring_paginate/util/LazyList.java
@@ -1,2 +1,213 @@
-package org.hls.check.check_spring_paginate.util;public class LazyList {
+package org.hls.check.check_spring_paginate.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class LazyList<E> implements List<E> {
+
+    private List<E> list = new ArrayList<>();
+    private transient boolean running;
+
+    // ----------------------------------------------------------------------
+    //
+    // ----------------------------------------------------------------------
+
+    public LazyList() {
+        super();
+        running = true;
+    }
+
+    // ----------------------------------------------------------------------
+    //
+    // ----------------------------------------------------------------------
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void done() {
+        running = false;
+    }
+
+    // ----------------------------------------------------------------------
+    // Important methods
+    // ----------------------------------------------------------------------
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean add(E e) {
+        synchronized (list) {
+            return list.add(e);
+        }
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        synchronized (list) {
+            return list.addAll(c);
+        }
+    }
+
+    // ----------------------------------------------------------------------
+    // Secondary methods
+    // ----------------------------------------------------------------------
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return list.addAll(index, c);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return list.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return list.retainAll(c);
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<E> operator) {
+        list.replaceAll(operator);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return list.listIterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return list.listIterator(index);
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return list.subList(fromIndex, toIndex);
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return list.spliterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        list.sort(c);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return list.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return list.hashCode();
+    }
+
+    @Override
+    public E get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return list.set(index, element);
+    }
+
+    @Override
+    public void add(int index, E element) {
+        list.add(index, element);
+    }
+
+    @Override
+    public E remove(int index) {
+        return list.remove(index);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super E> filter) {
+        return list.removeIf(filter);
+    }
+
+    @Override
+    public Stream<E> stream() {
+        return list.stream();
+    }
+
+    @Override
+    public Stream<E> parallelStream() {
+        return list.parallelStream();
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        list.forEach(action);
+    }
+
 }
