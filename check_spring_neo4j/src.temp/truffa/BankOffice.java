a76d36153e3f239cfd26c1b61e4818b46cbd623f
Corrado Mio <corrado.mio@gmail.com>
2020-11-02 09:02:28 +0400

    Updated implementation

---
+++ check_spring_neo4j/src.temp/truffa/BankOffice.java
@@ -0,0 +1,26 @@
+package org.hls.check.check_spring_neo4j.truffa;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity(label = "bank_office")
+public class BankOffice {
+
+    @Id
+    @GeneratedValue
+    private Long nativeId;
+
+    private String bank;
+    private String id;
+
+    public BankOffice() { }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return bank;
+    }
+}
