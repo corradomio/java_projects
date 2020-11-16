8cd737780479a70b1677a8cdeb9837d61b5545aa
Corrado Mio <corrado.mio@unimi.it>
2020-11-16 19:18:12 +0400

    Update implementation

--- jext.csv/src/main/java/jext/dataframe/data/DoubleData.java
+++ jext.csv/src/main/java/jext/dataframe/data/DoubleData.java
@@ -1,2 +1,19 @@
-package jext.dataframe.data;public class DoubleData {
+package jext.dataframe.data;
+
+import jext.dataframe.Data;
+
+public class DoubleData implements Data<Double> {
+
+    private double[] data;
+    private int n;
+
+    public DoubleData() {
+        data = new double[0];
+        n = 0;
+    }
+
+    public DoubleData(double[] data) {
+        this.data = data;
+        this.n = data.length;
+    }
 }
