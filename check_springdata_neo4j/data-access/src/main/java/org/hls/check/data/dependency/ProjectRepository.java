bb856cee2ef8cf44bf73c1ae7d5c31b40b30609a
Corrado Mio <corrado.mio@unimi.it>
2020-11-17 17:06:05 +0400

    Update implementation

--- check_springdata_neo4j/data-access/src/main/java/org/hls/check/data/dependency/ProjectRepository.java
+++ check_springdata_neo4j/data-access/src/main/java/org/hls/check/data/dependency/ProjectRepository.java
@@ -1,6 +1,6 @@
 package org.hls.check.data.dependency;
 
-import org.springframework.data.neo4j.repository.Neo4jRepository;
+import jext.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface ProjectRepository extends Neo4jRepository<Project, Long> {
 
