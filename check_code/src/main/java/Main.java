f273ad7899ec20ff1b1d2ddf5b48b17c8644f08e
Corrado Mio <corrado.mio@gmail.com>
2020-12-13 09:10:07 +0400

    Updated implementation

--- check_code/src/main/java/Main.java
+++ check_code/src/main/java/Main.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Main {
+class C {
+    public void m() { }
+    public void p() {}
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 0;
+
+        C c = new C();
+
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+        c.m();
+
+        c.m(); // 1                     // 1
+        c.m(); // 2                     //
+        for (int i=0; i<n; ++i) {       //
+            c.m();      // 3            //
+        }
+        c.m(   );  // 4                 //
+
+        c.p();                          // 2
+        c.m();                          // 3<<<   ? or skip ?
+
+
+    }
 }
