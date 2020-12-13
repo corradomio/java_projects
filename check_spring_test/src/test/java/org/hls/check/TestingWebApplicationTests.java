cef1e8ac04f3a2e135d5e7f1ff23f5d31123a1e6
Corrado Mio <corrado.mio@gmail.com>
2020-12-13 09:09:59 +0400

    Updated implementation

---
+++ check_spring_test/src/test/java/org/hls/check/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hls.check;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
