d5656e710675db1f864ff7b50825b5746fbc60a1
Corrado Mio <corrado.mio@gmail.com>
2021-01-24 20:30:20 +0400

    Updated implementation

--- check_code/src/main/java/Main.java
+++ check_code/src/main/java/Main.java
@@ -1,47 +1,37 @@
-class C {
-    public void m() { }
-    public void p() {}
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+class Account {
+    int code;
+
+    Account(int c) {
+        code = c;
+    }
+
+    int getCode() {
+        return code;
+    }
 }
 
 
 
 public class Main {
 
+    public static <T> Predicate<T> distinctByKey(
+        Function<? super T, ?> keyExtractor) {
+
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
     public static void main(String[] args) {
-        int n = 0;
-
-        C c = new C();
-
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-        c.m();
-
-        c.m(); // 1                     // 1
-        c.m(); // 2                     //
-        for (int i=0; i<n; ++i) {       //
-            c.m();      // 3            //
-        }
-        c.m(   );  // 4                 //
-
-        c.p();                          // 2
-        c.m();                          // 3<<<   ? or skip ?
-
-
+        List<Account> myList = Arrays.asList(new Account(1), new Account(3), new Account(2), new Account(3), new Account(1));
+        List<Account> response = myList.stream().filter(distinctByKey(Account::getCode)).collect(Collectors.toList());
+        System.out.println(response);
     }
 }
