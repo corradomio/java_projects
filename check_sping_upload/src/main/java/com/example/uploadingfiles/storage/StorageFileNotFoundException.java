500f9d4b48d760079b567553b940b78efdecb259
Corrado Mio <corrado.mio@gmail.com>
2021-01-24 20:29:31 +0400

    Updated implementation

--- check_sping_upload/src/main/java/com/example/uploadingfiles/storage/StorageFileNotFoundException.java
+++ check_sping_upload/src/main/java/com/example/uploadingfiles/storage/StorageFileNotFoundException.java
@@ -1,2 +1,12 @@
-package com.example.uploadingfiles.storage;public class StorageFileNotFoundException {
+package com.example.uploadingfiles.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
