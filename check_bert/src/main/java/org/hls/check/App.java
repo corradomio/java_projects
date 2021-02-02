3331d67ea34bcb70d0d833d33f64c0c6b69fa681
Corrado Mio <corrado.mio@gmail.com>
2021-02-02 10:40:09 +0400

    Updated implementation

--- check_bert/src/main/java/org/hls/check/App.java
+++ check_bert/src/main/java/org/hls/check/App.java
@@ -1,2 +1,29 @@
-package org.hls.check;public class App {
+package org.hls.check;
+
+import org.tensorflow.ConcreteFunction;
+import org.tensorflow.Signature;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+import org.tensorflow.op.Ops;
+import org.tensorflow.op.core.Placeholder;
+import org.tensorflow.op.math.Add;
+import org.tensorflow.types.TInt32;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello TensorFlow " + TensorFlow.version());
+
+        try (ConcreteFunction dbl = ConcreteFunction.create(App::dbl);
+             Tensor<TInt32> x = TInt32.scalarOf(10);
+             Tensor<TInt32> dblX = dbl.call(x).expect(TInt32.DTYPE)) {
+            System.out.println(x.data().getInt() + " doubled is " + dblX.data().getInt());
+        }
+    }
+
+    private static Signature dbl(Ops tf) {
+        Placeholder<TInt32> x = tf.placeholder(TInt32.DTYPE);
+        Add<TInt32> dblX = tf.math.add(x, x);
+        return Signature.builder().input("x", x).output("dbl", dblX).build();
+    }
 }
