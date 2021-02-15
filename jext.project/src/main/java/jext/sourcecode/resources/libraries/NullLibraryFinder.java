e8e1e7e8f1b72055caaa52ebf721fb1da6ccbeb0
Corrado Mio <corrado.mio@gmail.com>
2021-02-15 16:03:26 +0400

    Updated implementation

---
+++ jext.project/src/main/java/jext/sourcecode/resources/libraries/NullLibraryFinder.java
@@ -0,0 +1,42 @@
+package jext.sourcecode.resources.libraries;
+
+import jext.maven.MavenCoords;
+import jext.maven.MavenDownloader;
+import jext.sourcecode.project.Library;
+import jext.sourcecode.project.LibraryFinder;
+import jext.sourcecode.project.Project;
+import jext.util.Parameters;
+
+public class NullLibraryFinder implements LibraryFinder {
+
+    private static final LibraryFinder INSTANCE = new NullLibraryFinder();
+
+    public static LibraryFinder instance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public LibraryFinder setProject(Project project) {
+        return this;
+    }
+
+    @Override
+    public LibraryFinder configure(Parameters params) {
+        return this;
+    }
+
+    @Override
+    public Library getLibrary(MavenCoords coords) {
+        return null;
+    }
+
+    @Override
+    public Library getLibrary(String libraryName) {
+        return null;
+    }
+
+    @Override
+    public MavenDownloader getDownloader() {
+        return null;
+    }
+}
