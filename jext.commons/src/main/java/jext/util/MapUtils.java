0bffc97a2e914423d0a52ab5e4516eda1ff35648
Corrado Mio <corrado.mio@gmail.com>
2021-03-01 20:59:56 +0400

    Updated implementation

---
+++ jext.commons/src/main/java/jext/util/MapUtils.java
@@ -0,0 +1,33 @@
+package jext.util;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapUtils {
+
+    public static <K, V> Map<K, V> asMap(Object ... args) {
+        return fillMap(new HashMap<>(), args);
+    }
+
+    public static <K, V> Map<K, V> asTreeMap(Object ... args) {
+        return fillMap(new TreeMap<>(), args);
+    }
+
+    private static <K, V> Map<K, V> fillMap(Map<K, V> map, Object[] args) {
+        if (args == null || args.length == 0)
+            return Collections.emptyMap();
+
+        int n = args.length - (args.length%2);
+        for (int i=0; i<n; i+=2) {
+            K key = (K) args[i];
+            V value = (V) args[i+1];
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+}
---
+++ jext.commons/src/main/java/jext/util/TimeUtils.java
@@ -0,0 +1,67 @@
+package jext.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtils {
+
+    private static final SimpleDateFormat ID_FORMAT   = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+
+
+    public static String formatId(Date date) {
+        return ID_FORMAT.format(date);
+    }
+
+    public static String format(Date date){
+        return DATE_FORMAT.format(date);
+    }
+
+    /**
+     * interval ::= value                   -- in millis
+     *           |  value unit              -- in unit ('ms', 's')
+     *           |  value (':' value)+       -- mm:ss | hh:mm:ss | dd:hh:mm:ss
+     * @param interval
+     * @return
+     */
+    public static long toMillis(String interval) {
+        int n = interval.length();
+        long time = 0;
+        if(interval.contains(":")) {
+            String[] parts = interval.split(":");
+            switch (parts.length) {
+                case 4: // dd:hh:mm:ss
+                    time = ((Long.parseLong(parts[0])*24 +
+                             Long.parseLong(parts[1]))*60 +
+                             Long.parseLong(parts[2]))*60 +
+                             Long.parseLong(parts[3]);
+                    break;
+                case 3: // hh:mm:ss
+                    time = (Long.parseLong(parts[0])*60 +
+                            Long.parseLong(parts[1]))*60 +
+                            Long.parseLong(parts[2]);
+                    break;
+                default: // mm:ss
+                    time =  Long.parseLong(parts[0])*60 +
+                            Long.parseLong(parts[1]);
+                    break;
+            }
+
+            return time*1000;
+        }
+        else if (interval.endsWith("ms"))
+            return Long.parseLong(interval.substring(0, n-2));
+        else if (interval.endsWith("s"))
+            return Long.parseLong(interval.substring(0, n-1))*1000;
+        else
+            return Long.parseLong(interval);
+    }
+
+    public static String toInterval(long millis) {
+        long temp = (millis + 500)/1000;
+        long seconds = temp %60;
+        long minutes = temp / 60;
+
+        return String.format("%d:%02d", minutes, seconds);
+    }
+}
