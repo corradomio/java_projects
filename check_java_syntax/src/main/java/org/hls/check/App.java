6ac0bda46f8c7ba59d7edadc0596e6f1b7826170
Corrado Mio <corrado.mio@gmail.com>
2021-03-07 16:06:50 +0400

    Updated implementation

--- check_java_syntax/src/main/java/org/hls/check/App.java
+++ check_java_syntax/src/main/java/org/hls/check/App.java
@@ -1,2 +1,16 @@
-package org.hls.check;public class App {
+package org.hls.check;
+
+public class App {
+    static class StaticInner { }
+    class Inner { }
+
+    interface Itfc {
+
+    }
+
+    public static void main(String[] args) {
+
+        Itfc i = new Itfc() { };
+
+    }
 }
