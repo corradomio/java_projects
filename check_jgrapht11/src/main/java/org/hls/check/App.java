17aa3717c33f47ea5eef0f2e978f0bea1ac336f4
Corrado Mio <corrado.mio@gmail.com>
2021-03-31 20:15:24 +0400

    Updated implementation

--- check_jgrapht11/src/main/java/org/hls/check/App.java
+++ check_jgrapht11/src/main/java/org/hls/check/App.java
@@ -1,10 +1,18 @@
 package org.hls.check;
 
+import jext.jgrapht.Graphs;
+import jext.jgrapht.util.GraphDump;
+import org.jgrapht.Graph;
 import org.jgrapht.generate.ScaleFreeGraphGenerator;
+import org.jgrapht.graph.DefaultEdge;
 
 public class App {
 
     public static void main(String[] args) {
-        new ScaleFreeGraphGenerator<Integer, Edge>()
+        Graph<Integer, DefaultEdge> g = Graphs.newGraph(true, false);
+        new ScaleFreeGraphGenerator<Integer, DefaultEdge>(50000)
+            .generateGraph(g);
+
+        GraphDump.printGraphInfo(g);
     }
 }
