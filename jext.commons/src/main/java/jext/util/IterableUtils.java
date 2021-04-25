248b022108c3435e2e0d600dfe970f2ef183fcb4
Corrado Mio <corrado.mio@gmail.com>
2021-04-25 13:23:13 +0400

    Updated implementation

--- jext.commons/src/main/java/jext/util/IterableUtils.java
+++ jext.commons/src/main/java/jext/util/IterableUtils.java
@@ -1,6 +1,7 @@
 package jext.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
--- jext.commons/src/main/java/jext/util/JarUtils.java
+++ jext.commons/src/main/java/jext/util/JarUtils.java
@@ -10,6 +10,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.jar.JarEntry;
