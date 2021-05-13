d2499313b763ca2dc4186589eedf25125f64233e
Corrado Mio <corrado.mio@gmail.com>
2021-05-13 11:18:18 +0400

    Update implementation

---
+++ jext.versioning/src/main/java/jext/versioning/ftp/RemoteFile.java
@@ -0,0 +1,11 @@
+package jext.versioning.ftp.util;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+public class RemoteFile {
+
+    private FTPClient client;
+    private FTPFile ftpFile;
+    private String path;
+}
