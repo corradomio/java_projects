b26cbfa5351f42c367bc9df6ef202d1f27c2bb3b
Corrado Mio <corrado.mio@gmail.com>
2021-06-06 15:20:07 +0400

    Updated implementation

---
+++ jext.javaparser/src/main/java/jext/javaparser/symbolsolver/resolution/typesolvers/JavaRuntimeTypeSolver.java
@@ -0,0 +1,25 @@
+package jext.javaparser.symbolsolver.resolution.typesolvers;
+
+import jext.javaparser.util.ClassPoolRegistry;
+
+/**
+ * Used to assign a reasonable class name to the TypeSolver
+ */
+public class JavaRuntimeTypeSolver extends ClassPoolRegistryTypeSolver {
+
+    public JavaRuntimeTypeSolver() {
+        super();
+    }
+
+    public JavaRuntimeTypeSolver(ClassPoolRegistry classPoolRegistry) {
+        super(classPoolRegistry);
+    }
+
+    public JavaRuntimeTypeSolver(String name) {
+        super(name);
+    }
+
+    public JavaRuntimeTypeSolver(String name, ClassPoolRegistry classPoolRegistry) {
+        super(name, classPoolRegistry);
+    }
+}
---
+++ jext.javaparser/src/main/java/jext/javaparser/symbolsolver/resolution/typesolvers/LibrariesTypeSolver.java
@@ -0,0 +1,24 @@
+package jext.javaparser.symbolsolver.resolution.typesolvers;
+
+import jext.javaparser.util.ClassPoolRegistry;
+
+/**
+ * Used to assign a reasonable class name to the TypeSolver
+ */
+public class LibrariesTypeSolver extends ClassPoolRegistryTypeSolver {
+    public LibrariesTypeSolver() {
+        super();
+    }
+
+    public LibrariesTypeSolver(ClassPoolRegistry classPoolRegistry) {
+        super(classPoolRegistry);
+    }
+
+    public LibrariesTypeSolver(String name) {
+        super(name);
+    }
+
+    public LibrariesTypeSolver(String name, ClassPoolRegistry classPoolRegistry) {
+        super(name, classPoolRegistry);
+    }
+}
