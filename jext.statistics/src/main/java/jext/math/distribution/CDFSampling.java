07b9705ed360aa2f886330bde729fa7553c2b553
Corrado Mio <corrado.mio@gmail.com>
2021-06-08 18:09:32 +0400

    Updated implementation

--- jext.statistics/src/main/java/jext/math/distribution/CDFSampling.java
+++ jext.statistics/src/main/java/jext/math/distribution/CDFSampling.java
@@ -4,32 +4,38 @@ import jext.math.random.RandomGenerator;
 
 public class CDFSampler implements Sampler {
 
-    private Distribution distribution;
+    private final Distribution distribution;
     private RandomGenerator generator;
+    private final double eps = 1.e-8;
 
     public CDFSampler(Distribution cd, RandomGenerator rg) {
         this.distribution = cd;
         this.generator = rg;
     }
 
-
     @Override
     public double sample() {
         double r = generator.nextDouble();
+        return sample(r);
+    }
+
+    public double sample(double r) {
         double a = distribution.lowerLimit();
         double b = distribution.upperLimit();
+
+        double p = 0; // prev value
         double c = 0.5*(a+b);
-        double p = 0;
         double f = distribution.cdf(c);
-        double eps = 1.e-8;
 
         while ((b-a) > eps || (f-p) > eps) {
+            p = f;
             if (f < r)
+                // upper half
                 a = c;
             else
+                // lower half
                 b = c;
             c = (a+b)/2;
-            p = f;
             f = distribution.cdf(c);
         }
         return c;
