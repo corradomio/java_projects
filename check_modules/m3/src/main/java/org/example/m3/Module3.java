14c1e948da638b7a8a6203f244e33f4e64f78223
Corrado Mio <corrado.mio@gmail.com>
2021-07-04 15:27:02 +0400

    Updated implementation

--- check_modules/m3/src/main/java/org/example/m3/Module3.java
+++ check_modules/m3/src/main/java/org/example/m3/Module3.java
@@ -1,17 +1,9 @@
 package org.example.m3;
 
-import org.example.m2.Module2;
-
 public class Module3 {
 
-    Module2 m2;
-
-    // public void test() {
-    //     System.out.printf("%s.test()", getClass().getName());
-    // }
-
-    public String test3(String who) {
-        m2.test();
-        return "";
+    public void test() {
+        System.out.printf("%s.test()", getClass().getName());
     }
+
 }
--- jext.project/src/main/java/jext/sourcecode/project/TypeUse.java
+++ jext.project/src/main/java/jext/sourcecode/project/TypeUse.java
@@ -1,6 +1,7 @@
 package jext.sourcecode.project;
 
 public enum TypeUse {
+    NONE,       // no type use defined
     ALL,        // EXTENDS or IMPLEMENTS or DEPENDS_ON
     HIERARCHY,  // EXTENDS or IMPLEMENTS
     EXTENDS,
