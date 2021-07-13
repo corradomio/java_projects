242c6b8c148c89eb39366d78e12a6ed707974da4
Corrado Mio <corrado.mio@gmail.com>
2021-07-13 19:18:27 +0400

    Updated implementation

---
+++ jext.project/src/main/java/jext/sourcecode/project/util/DefinedMethod.java
@@ -0,0 +1,149 @@
+package jext.sourcecode.project.util;
+
+import jext.name.NamedObject;
+import jext.sourcecode.project.Method;
+import jext.sourcecode.project.MethodName;
+import jext.sourcecode.project.Parameter;
+import jext.sourcecode.project.RefType;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static jext.lang.JavaUtils.PUBLIC;
+
+public class DefinedMethod extends NamedObject implements Method {
+
+    private String declaration;
+    private RefType ownerType;
+    private RefType returnType = ReferencedType.JAVA_LANG_VOID;
+    private List<Parameter> parameters = new ArrayList<>();
+    private AtomicInteger callIndex;
+    private MethodName lastCallName;
+
+    // ----------------------------------------------------------------------
+    // Constructor
+    // ----------------------------------------------------------------------
+
+    public DefinedMethod(RefType ownerType, String methodName, int nParams) {
+        this(ownerType, methodName, nParams, null, null);
+    }
+
+    public DefinedMethod(RefType ownerType,
+                         String methodName, int nParams,
+                         String signature, String declaration) {
+        super(new MethodNameObject(ownerType.getName(), methodName, nParams, toSignature(methodName, nParams, signature)));
+
+        // remove spaces
+        if (declaration != null)
+            declaration = declaration.trim();
+        this.ownerType = ownerType;
+        this.callIndex = new AtomicInteger();
+        this.lastCallName = getMethodName();
+        this.declaration = declaration;
+    }
+
+    private static String toSignature(String methodName, int nParams, String signature) {
+        // methodName = StringUtils.lastOf(methodName, ".");
+        // if (signature != null)
+        //     return JavaUtils.toPlainMethodSignature(signature);
+        // else
+        //     return JavaUtils.toPlainMethodSignature(methodName, nParams);
+        return signature;
+    }
+
+    // ----------------------------------------------------------------------
+    // Setters
+    // ----------------------------------------------------------------------
+
+    public void setReturnType(RefType returnType) {
+        if (returnType != null)
+            this.returnType = returnType;
+        else
+            this.returnType = ReferencedType.JAVA_LANG_VOID;
+    }
+
+    public void add(String parameterName, RefType type) {
+        MethodParameter parameter = new MethodParameter(this, parameterName);
+        parameter.setType(type);
+        parameters.add(parameter);
+    }
+
+    // ----------------------------------------------------------------------
+    // Properties
+    // ----------------------------------------------------------------------
+
+    @Override
+    public MethodName getMethodName() {
+        return (MethodName) getName();
+    }
+
+    @Override
+    public Set<String> getModifiers() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getVisibility() {
+        return PUBLIC;
+    }
+
+    @Override
+    public Set<String> getStructure() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public RefType getType() {
+        return returnType;
+    }
+
+    @Override
+    public String getTypeId() {
+        return returnType.getId();
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        return parameters;
+    }
+
+    @Override
+    public RefType getOwnerType() {
+        return ownerType;
+    }
+
+    @Override
+    public String getOwnerTypeId() {
+        return ownerType.getId();
+    }
+
+    @Override
+    public String getSignature() {
+        return getMethodName().getSignature();
+    }
+
+    @Override
+    public String getDeclaration() {
+        return declaration;
+    }
+
+    @Override
+    public long getDigest() {
+        return 0;
+    }
+
+    // ----------------------------------------------------------------------
+    // Operations
+    // ----------------------------------------------------------------------
+
+    public Optional<Integer> nextIndex(Method lastCall) {
+        if (lastCallName.equals(lastCall.getMethodName()))
+            return Optional.empty();
+        lastCallName = lastCall.getMethodName();
+        return Optional.of(callIndex.getAndIncrement());
+    }
+}
---
+++ jext.project/src/main/java/jext/sourcecode/project/util/ReferencedType.java
@@ -0,0 +1,162 @@
+package jext.sourcecode.project.util;
+
+import jext.java.TypeRole;
+import jext.lang.JavaUtils;
+import jext.logging.Logger;
+import jext.name.Name;
+import jext.name.NamedObject;
+import jext.name.ObjectName;
+import jext.sourcecode.project.Library;
+import jext.sourcecode.project.Project;
+import jext.sourcecode.project.RefType;
+import jext.sourcecode.project.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReferencedType extends NamedObject implements RefType {
+
+    // ----------------------------------------------------------------------
+    // Constants
+    // ----------------------------------------------------------------------
+
+    public static final ReferencedType JAVA_LANG_NULL = new ReferencedType(JavaUtils.JAVA_LANG_NULL);
+    public static final ReferencedType JAVA_LANG_VOID = new ReferencedType(JavaUtils.JAVA_LANG_VOID);
+    public static final ReferencedType JAVA_LANG_OBJECT = new ReferencedType(JavaUtils.JAVA_LANG_OBJECT);
+    public static final ReferencedType JAVA_LANG_CLASS = new ReferencedType(JavaUtils.JAVA_LANG_CLASS);
+    // public static final ReferencedType ARRAY = new ReferencedType(JavaUtils.ARRAY);
+
+    // ----------------------------------------------------------------------
+    // Fields
+    // ----------------------------------------------------------------------
+
+    protected static Logger logger = Logger.getLogger(ReferencedType.class);
+
+    public Name namespace;
+    public int nTypeParams;
+    public TypeRole role;
+    public Library library;
+    private List<RefType> elements = new ArrayList<>();
+
+    // ----------------------------------------------------------------------
+    // Constructor
+    // ----------------------------------------------------------------------
+
+    public ReferencedType(String fullName) {
+        this(new ObjectName(fullName));
+    }
+
+    public ReferencedType(String fullName, Library library) {
+        this(new ObjectName(fullName), library);
+    }
+
+    public ReferencedType(Name name) {
+        this(name, null);
+    }
+
+    public ReferencedType(Name name, Library library) {
+        super(name);
+        this.library = library;
+        this.role = TypeRole.UNKNOWN;
+        this.namespace = guessNamespace();
+
+        // DEBUG
+        String fullname = name.getFullName();
+        if (fullname.contains("<") || fullname.contains("[") || fullname.contains("\\") || fullname.contains("?"))
+            Logger.getLogger(ReferencedType.class).errorf("Invalid: %s", fullname);
+        if (JavaUtils.isPrimitive(fullname)) {
+            setName(JavaUtils.boxed(fullname));
+        }
+    }
+
+    private Name guessNamespace() {
+        String fullname = getName().getFullName();
+        int lst = fullname.length();
+        int dot = fullname.lastIndexOf('.');
+        while (dot != -1 && Character.isUpperCase(fullname.charAt(dot+1))) {
+            lst = dot;
+            dot = fullname.lastIndexOf('.', dot-1);
+        }
+        if (dot != -1) {
+            String namespace = fullname.substring(0, lst);
+            return new ObjectName(namespace);
+        }
+        else {
+            return new ObjectName(JavaUtils.ROOT);
+        }
+    }
+
+    // ----------------------------------------------------------------------
+    // Properties
+    // ----------------------------------------------------------------------
+
+    @Override
+    public Name getNamespace() {
+        return this.namespace;
+    }
+
+    @Override
+    public boolean isType() { return !role.equals(TypeRole.UNKNOWN); }
+
+    @Override
+    public boolean isAnonymous() {
+        return this.getName().getName().startsWith("Anonymous");
+    }
+
+    @Override
+    public boolean isCollection() {
+        return !elements.isEmpty();
+    }
+
+    @Override
+    public List<RefType> getElements() {
+        return elements;
+    }
+
+    @Override
+    public Type asType() {
+        throw new ClassCastException();
+    }
+
+    @Override
+    public int getTypeParametersCount() {
+        return nTypeParams;
+    }
+
+    @Override
+    public TypeRole getRole() {
+        return role;
+    }
+
+    @Override
+    public Library getLibrary() {
+        return library;
+    }
+
+    @Override
+    public String getLibraryId() {
+        if (library != null)
+            return library.getId();
+        else
+            return null;
+    }
+
+    @Override
+    public Project getProject() {
+        return library.getProject();
+    }
+
+    // ----------------------------------------------------------------------
+    // Implementation
+    // ----------------------------------------------------------------------
+
+    public void add(RefType refType) {
+        elements.add(refType);
+    }
+
+    @Override
+    public String toString() {
+        return this.getName().getFullName();
+    }
+
+}
