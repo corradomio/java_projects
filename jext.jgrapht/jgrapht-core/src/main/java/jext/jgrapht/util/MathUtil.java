e1ccdbec0eef8b903e779123e802dceac4921273
Corrado Mio <corrado.mio@gmail.com>
2021-07-16 21:26:58 +0400

    Updated implementation

--- jext.jgrapht/jgrapht-core/src/main/java/jext/jgrapht/util/MathUtil.java
+++ jext.jgrapht/jgrapht-core/src/main/java/jext/jgrapht/util/MathUtil.java
@@ -1,6 +1,6 @@
 package jext.jgrapht.util;
 
-public class Mathx {
+public class MathUtils {
 
     public static long   sq(long x) { return x*x; }
     public static double sq(double x) { return x*x; }
@@ -67,4 +67,32 @@ public class Mathx {
         return s;
     }
 
+
+    public static double[] newVector(int n) {
+        return new double[n];
+    }
+
+    public static double[][] newMatrix(int n) {
+        return newMatrix(n, n);
+    }
+
+    public static double[][] newMatrix(int nr, int nc) {
+        double[][] m = new double[nr][];
+        for (int i=0; i<nr; ++i)
+            m[i] = new double[nc];
+        return m;
+    }
+
+    public static int[] intVector(int n) {
+        return new int[n];
+    }
+
+    public static int[][] intMatrix(int n) {return intMatrix(n, n); }
+
+    public static int[][] intMatrix(int nr, int nc) {
+        int[][] m = new int[nr][];
+        for (int i=0; i<nr; ++i)
+            m[i] = new int[nc];
+        return m;
+    }
 }
