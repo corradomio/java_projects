3e4f978d226b9a3ad4848a66f97b8cbd3b196bb8
Corrado Mio <corrado.mio@gmail.com>
2021-07-18 07:49:56 +0400

    Updated implementation

---
+++ check_jna/src/main/java/org/hls/check/Line.java
@@ -0,0 +1,10 @@
+package org.hls.check;
+
+import com.sun.jna.Structure;
+
+class Line extends Structure {
+    public Point start;
+    public Point end;
+
+    public Line() { }
+}
---
+++ check_jna/src/main/java/org/hls/check/Point2.java
@@ -0,0 +1,2 @@
+package org.hls.check;public class Point2 {
+}
