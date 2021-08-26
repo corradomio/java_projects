6cfd37bd890f2c45b5173aeca3fe03ec1a112a3c
Corrado Mio <corrado.mio@gmail.com>
2021-08-26 09:25:51 +0400

    Updated implementation

--- check_classloader/src/main/java/org/hls/check/Main.java
+++ check_classloader/src/main/java/org/hls/check/Main.java
@@ -1,2 +1,60 @@
-package org.hls.check;public class Main {
+package org.hls.check;
+
+/*
+    ClassLoader
+        SecureClassLoader
+            URLClassLoader
+                AppClassLoader
+                ExtClassLoader
+
+    class sun.misc.Launcher$ExtClassLoader          <== root classLoader
+        class sun.misc.Launcher$AppClassLoader      <== system classLoader
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+class C<T> implements Iterable<T> {
+
+    private List<T> l = new ArrayList<>();
+
+    public C(List<T> ll) {
+        l.addAll(ll);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return l.iterator();
+    }
+
+    public Iterable<T> iterateOn() {
+        return this;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        C<String> c = new C<>(Arrays.asList("1","2","3"));
+
+        for (String s : c.iterateOn()) {
+            System.out.println(s);
+        }
+
+
+    }
+
+    public static void main1(String[] args) {
+        ClassLoader cl = Main.class.getClassLoader();
+        int i=0;
+        while (cl != null) {
+            i++;
+            System.out.printf("%d) %s\n", i, cl.getClass().toString());
+            cl = cl.getParent();
+        }
+        System.out.printf("s) %s\n", ClassLoader.getSystemClassLoader().getClass().toString());
+
+    }
 }
--- jext.classloader/src/main/java/jext/lang/ClassLoaderConfiguration.java
+++ jext.classloader/src/main/java/jext/lang/ClassLoaderConfiguration.java
@@ -1,2 +1,67 @@
-package jext.lang;public class ClassLoaderConfiguration {
+package jext.lang;
+
+import java.io.File;
+import java.io.InvalidObjectException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassLoaderConfiguration {
+
+    private List<URL> urls = new ArrayList<>();
+
+
+    public ClassLoaderConfiguration add(File fileOrDirectory) throws MalformedURLException {
+        File[] libs;
+        String name = fileOrDirectory.getName().toLowerCase();
+        boolean isLibs = false;
+
+        if (!fileOrDirectory.exists())
+            return this;
+
+        if (fileOrDirectory.isFile()) {
+            // check if it is a jar or jmod
+            if (name.endsWith(".jar") || name.endsWith(".zip") || name.endsWith(".jmod"))
+                urls.add(fileOrDirectory.getAbsoluteFile().toURL());
+            // check if it is a .class
+            else if (name.endsWith(".class"))
+                urls.add(fileOrDirectory.getAbsoluteFile().getParentFile().toURL());
+        }
+        else if (fileOrDirectory.isDirectory()) {
+            isLibs = isLibs || addLibs(fileOrDirectory, ".jar");
+            isLibs = isLibs || addLibs(fileOrDirectory, ".zip");
+            isLibs = isLibs || addLibs(fileOrDirectory, ".jmod");
+
+            if (!isLibs)
+                urls.add(fileOrDirectory.getAbsoluteFile().toURL());
+        }
+        else {
+
+        }
+        // check if is a directory containing jars/jmods
+        // otherwise is a directory containing classes
+
+        return this;
+    }
+
+    private boolean addLibs(File directory, String ext) throws MalformedURLException {
+        File[] libs = directory.listFiles(file -> file.getName().endsWith(".zip"));
+        boolean isLibs =  libs != null && libs.length > 0;
+        if (isLibs) {
+            for(File lib : libs)
+                urls.add(lib.getAbsoluteFile().toURL());
+        }
+        return isLibs;
+    }
+
+    public ClassLoader build(ClassLoader parent) {
+        URL[] urls = new URL[this.urls.size()];
+        this.urls.toArray(urls);
+        if (parent == null)
+            return new URLClassLoader(urls);
+        else
+            return new URLClassLoader(urls, parent);
+    }
 }
