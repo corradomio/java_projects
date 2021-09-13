7f2e29981de45317c71e54643144d1776e10eea9
Corrado Mio <corrado.mio@unimi.it>
2021-09-13 15:34:10 +0400

    Updated implementation

---
+++ check_modules/main/src/main/java/org/example/main/Type1.java
@@ -0,0 +1,12 @@
+package org.example.main;
+
+public class Type1 {
+
+    // private Type2 t2;
+    private Type3 t3;
+
+    public Type1() {
+        // t2 = new Type2();
+        t3 = new Type3();
+    }
+}
---
+++ check_modules/main/src/main/java/org/example/main/Type2.java
@@ -0,0 +1,5 @@
+// package org.example.main;
+//
+// public class Type2 {
+//
+// }
