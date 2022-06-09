7ea12dc96a8f55a804d62a7e9e824b777815890d
Corrado Mio <corrado.mio@gmail.com>
2022-06-09 16:03:20 +0400

    Update implementation

--- jext.project/src/main/java/jext/sourcecode/project/lfm/RepositoryConfiguration.java
+++ jext.project/src/main/java/jext/sourcecode/project/lfm/RepositoryConfiguration.java
@@ -8,10 +8,42 @@ public class RepositoryConfiguration {
     private String url;
 
     public void configure(Configuration configuration) {
-        this.name = configuration.getString("@name", "");
-        this.url = configuration.getString("@url");
-        if (this.name.isEmpty())
-            this.name = this.url;
+        name = configuration.getString("@name", "");
+        url = configuration.getString("@url");
+        if (name.isEmpty())
+            urlToName();
+    }
+
+    private void urlToName() {
+        int b, e;
+        b = indexOf(":///");
+        if (b == -1)
+            b = indexOf("://");
+        if (b == -1)
+            b = indexOf(":/");
+        if (b == -1)
+            b = indexOf("/");
+        if (b == -1)
+            b = indexOf(":");
+
+        e = indexOf("/", b+1);
+
+        if (b != -1 && e != -1 && (b-e) > 1)
+            name = url.substring(b, e);
+        else
+            name = url;
+    }
+
+    private int indexOf(String pat) {
+        int p = url.indexOf(pat);
+        if (p != -1)
+            p += pat.length();
+        return p;
+    }
+
+    private int indexOf(String pat, int start) {
+        int p = url.indexOf(pat, start);
+        return p;
     }
 
     public String getName() {
